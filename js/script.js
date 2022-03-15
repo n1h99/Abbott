d33cff99b50ba088410ada284942a5c4124a21b7
pirma <pirmammadov.eldar@gmail.com>
2022-03-15 19:07:42 +0400

    add form js file

--- js/script.js
+++ js/script.js
@@ -4474,47 +4474,6 @@ if (!!modalFrut) {
     }
 }
 
-// validation
-
-const inputs = document.querySelectorAll('.validation__element')
-const selects = document.querySelectorAll('.validation__select')
-const form = document.querySelector('.information')
-
-if (!!form) {
-    form.addEventListener('submit', e => {
-        e.preventDefault()
-
-        inputs.forEach(item => {
-            if (item.value.length < 1)
-                item.parentNode.classList.add('label__error')
-
-            else
-                item.parentNode.classList.remove('label__error')
-
-        })
-
-        selects.forEach(select => {
-            if (select.value == '')
-                select.parentNode.classList.add('label__error')
-            else
-                select.parentNode.classList.remove('label__error')
-        })
-    })
-
-    form.addEventListener('input', e =>
-        e.target.parentNode.classList.remove('label__error'))
-
-    form.addEventListener('select', e =>
-        e.target.parentNode.classList.remove('label__error'))
-}
-
-if (!!document.querySelector('#UF_WHERE_LEARN')) {
-    document.querySelector('#UF_WHERE_LEARN').addEventListener('change',(e) =>  {
-        if (e.value === 'контактное лицо Эбботт') {
-            document.querySelector('#inputHidden').style.display = "block"
-        }
-    })
-}
 if (!!document.querySelector('.event-info__favorites')) {
     document.querySelector('.event-info__favorites').addEventListener('click', () => {
         document.querySelector('.event-info__favorites').classList.toggle('event-info__favorites--checked')
